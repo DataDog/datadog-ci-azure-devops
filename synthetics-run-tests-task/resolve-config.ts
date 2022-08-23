e98494f647b79a036cf0a3fcf306d926d96be223
corentin <corentin.girard@datadoghq.com>
2022-08-23 13:35:42 +0200

    Setup inputs and service connection

--- synthetics-run-tests-task/resolve-config.ts
+++ synthetics-run-tests-task/resolve-config.ts
@@ -1,8 +1,13 @@
-import {synthetics} from '@datadog/datadog-ci'
+import * as task from 'azure-pipelines-task-lib/task'
+
+import {synthetics, utils} from '@datadog/datadog-ci'
+import deepExtend from 'deep-extend'
+
+import {removeUndefinedValues} from './utils'
 
 const DEFAULT_CONFIG: synthetics.CommandConfig = {
-  apiKey: process.env.DATADOG_API_KEY || '',
-  appKey: process.env.DATADOG_APP_KEY || '',
+  apiKey: '',
+  appKey: '',
   configPath: 'datadog-ci.json',
   datadogSite: 'datadoghq.com',
   failOnCriticalErrors: false,
@@ -18,6 +23,87 @@ const DEFAULT_CONFIG: synthetics.CommandConfig = {
   variableStrings: [],
 }
 
+function resolveKeys(): {apiKey: string; appKey: string} {
+  const authenticationType = task.getInput('authenticationType')
+
+  switch (authenticationType) {
+    case 'connectedService':
+      try {
+        const serviceId = task.getInputRequired('connectedService')
+        const apiKey = task.getEndpointAuthorizationParameterRequired(serviceId, 'apitoken')
+        const appKey = task.getEndpointAuthorizationParameterRequired(serviceId, 'appkey')
+        return {apiKey, appKey}
+      } catch (error) {
+        task.setResult(
+          task.TaskResult.Failed,
+          'Missing API or APP keys to initialize datadog-ci! Check your Datadog service connection.'
+        )
+        throw error
+      }
+
+    case 'apiAppKeys':
+      try {
+        const apiKey = task.getInputRequired('apiKey')
+        const appKey = task.getInputRequired('appKey')
+        return {apiKey, appKey}
+      } catch (error) {
+        task.setResult(task.TaskResult.Failed, 'Missing API or APP keys to initialize datadog-ci!')
+        throw error
+      }
+
+    default:
+      task.setResult(task.TaskResult.Failed, 'Unknown authentication type')
+      throw Error('Unknown authentication type')
+  }
+}
+
 export const resolveConfig = async (): Promise<synthetics.CommandConfig> => {
-  return DEFAULT_CONFIG
+  const {apiKey, appKey} = resolveKeys()
+
+  const publicIds = task
+    .getInput('publicIds')
+    ?.split(',')
+    .map((publicId: string) => publicId.trim())
+  const datadogSite = task.getInput('datadogSite')
+  const configPath = task.getPathInput('configPath')
+  const files = task
+    .getInput('files')
+    ?.split(',')
+    .map((file: string) => file.trim())
+  const testSearchQuery = task.getInput('testSearchQuery')
+  const subdomain = task.getInput('subdomain')
+  const variableStrings = task
+    .getInput('variables')
+    ?.split(',')
+    .map((variableString: string) => variableString.trim())
+
+  let config = JSON.parse(JSON.stringify(DEFAULT_CONFIG))
+  // Override with file config variables
+  try {
+    config = await utils.resolveConfigFromFile(config, {configPath, defaultConfigPath: DEFAULT_CONFIG.configPath})
+  } catch (error) {
+    if (configPath) {
+      task.setResult(task.TaskResult.Failed, `Unable to parse config file! Please verify config path : ${configPath}`)
+      throw error
+    }
+    // Here, if configPath is not present it means that default config file does not exist: in this case it's expected for the task to be silent.
+  }
+
+  // Override with the task's inputs
+  config = deepExtend(
+    config,
+    removeUndefinedValues({
+      apiKey,
+      appKey,
+      configPath,
+      datadogSite,
+      files,
+      publicIds,
+      subdomain,
+      testSearchQuery,
+      variableStrings,
+    })
+  )
+
+  return config
 }
