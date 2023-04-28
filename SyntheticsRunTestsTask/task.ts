ddc11858d21ba67b4c9d6fcef6ff85a56e8bfa9f
corentin <corentin.girard@datadoghq.com>
2023-04-28 13:48:12 +0200

    Review comments

--- SyntheticsRunTestsTask/task.ts
+++ SyntheticsRunTestsTask/task.ts
@@ -13,11 +13,25 @@ async function run(): Promise<void> {
   const config = await resolveConfig(reporter)
   const startTime = Date.now()
 
-  let results: synthetics.Result[]
-  let summary: synthetics.Summary
-
   try {
-    ;({results, summary} = await synthetics.executeTests(reporter, config))
+    const {results, summary} = await synthetics.executeTests(reporter, config)
+    const orgSettings = await synthetics.utils.getOrgSettings(reporter, config)
+
+    synthetics.utils.renderResults({
+      config,
+      orgSettings,
+      reporter,
+      results,
+      startTime,
+      summary,
+    })
+
+    synthetics.utils.reportExitLogs(reporter, config, {results})
+
+    const exitReason = synthetics.utils.getExitReason(config, {results})
+    if (exitReason !== 'passed') {
+      task.setResult(task.TaskResult.Failed, `Datadog Synthetics tests failed. ${printSummary(summary, config)}`)
+    }
   } catch (error) {
     synthetics.utils.reportExitLogs(reporter, config, {error})
 
@@ -25,24 +39,16 @@ async function run(): Promise<void> {
     if (exitReason !== 'passed') {
       task.setResult(task.TaskResult.Failed, 'Running Datadog Synthetics tests failed.')
     }
-    return
   }
+}
 
-  const orgSettings = await synthetics.utils.getOrgSettings(reporter, config)
-
-  synthetics.utils.renderResults({
-    config,
-    orgSettings,
-    reporter,
-    results,
-    startTime,
-    summary,
-  })
-
-  const exitReason = synthetics.utils.getExitReason(config, {results})
-  if (exitReason !== 'passed') {
-    task.setResult(task.TaskResult.Failed, 'Datadog Synthetics tests failed.')
-  }
+export const printSummary = (summary: synthetics.Summary, config: synthetics.SyntheticsCIConfig): string => {
+  const baseUrl = synthetics.utils.getAppBaseURL(config)
+  const batchUrl = synthetics.utils.getBatchUrl(baseUrl, summary.batchId)
+  return (
+    `criticalErrors: ${summary.criticalErrors}, passed: ${summary.passed}, failedNonBlocking: ${summary.failedNonBlocking}, failed: ${summary.failed}, skipped: ${summary.skipped}, notFound: ${summary.testsNotFound.size}, timedOut: ${summary.timedOut}\n` +
+    `Results URL: ${batchUrl}`
+  )
 }
 
 void run().catch(e => {
