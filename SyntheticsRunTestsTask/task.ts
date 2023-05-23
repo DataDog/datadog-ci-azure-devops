93516639ec50418c007b655bbbe4430653d6adeb
corentin <corentin.girard@datadoghq.com>
2023-05-23 13:48:08 +0200

    this is required apparently

--- SyntheticsRunTestsTask/task.ts
+++ SyntheticsRunTestsTask/task.ts
@@ -9,7 +9,7 @@ async function run(): Promise<void> {
   task.setResourcePath(path.join(__dirname, 'task.json'))
   synthetics.utils.setCiTriggerApp('azure_devops_task')
 
-  console.log('hello you!')
+  console.log('hello you 2!')
 
   const reporter = getReporter()
   const config = await resolveConfig(reporter)
