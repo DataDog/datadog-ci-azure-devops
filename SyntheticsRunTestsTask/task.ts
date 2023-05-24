18467ebfe715510b57b96be00d19fcb92584ab60
corentin <corentin.girard@datadoghq.com>
2023-05-24 18:01:29 +0200

    cleanup and try again without patching the task

--- SyntheticsRunTestsTask/task.ts
+++ SyntheticsRunTestsTask/task.ts
@@ -9,7 +9,7 @@ async function run(): Promise<void> {
   task.setResourcePath(path.join(__dirname, 'task.json'))
   synthetics.utils.setCiTriggerApp('azure_devops_task')
 
-  console.log('hello you! 0.1.98')
+  console.log('hello you! not patching task')
 
   const reporter = getReporter()
   const config = await resolveConfig(reporter)
