166cf07dd53f43613182264168ad8caa1025375f
corentin <corentin.girard@datadoghq.com>
2023-11-14 15:15:01 +0100

    [dep] Remove superfluous libraries

--- SyntheticsRunTestsTask/task.node10.ts
+++ SyntheticsRunTestsTask/task.node10.ts
@@ -1,9 +1,8 @@
-import {logError, LogType} from 'azure-pipelines-tasks-packaging-common/util'
+import * as task from 'azure-pipelines-task-lib/task'
 
-logError(
+task.warning(
   'Node 10 support for SyntheticsRunTestsTask is deprecated, please upgrade the agent version.\n' +
-    'See https://learn.microsoft.com/en-us/azure/devops/pipelines/agents/agents#agent-version-and-upgrades',
-  LogType.warning
+    'See https://learn.microsoft.com/en-us/azure/devops/pipelines/agents/agents#agent-version-and-upgrades'
 )
 
 require('./task')
