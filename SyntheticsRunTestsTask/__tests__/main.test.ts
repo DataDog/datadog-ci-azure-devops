1f92b7ca3055dd5e7ffb3bfe15b8a9ec4d1a2b48
Corentin Girard <corentin.girard@datadoghq.com>
2025-04-29 11:14:08 +0200

    [chores] Make unit tests clearer (#216)
    * Rename "mocks" to "scenarios"
    
    * Fixup bad braces bump

--- SyntheticsRunTestsTask/__tests__/main.test.ts
+++ SyntheticsRunTestsTask/__tests__/main.test.ts
@@ -2,56 +2,44 @@ import {synthetics} from '@datadog/datadog-ci'
 
 import * as fs from 'fs'
 
-import {
-  BASE_INPUTS,
-  CUSTOM_PUBLIC_IDS,
-  CUSTOM_SITE,
-  CUSTOM_SUBDOMAIN,
-  expectSpy,
-  runMockTaskApiKeys,
-  runMockTaskJUnitReport,
-  runMockTaskPollingTimeout,
-  runMockTaskServiceConnection,
-  runMockTaskServiceConnectionEnvVars,
-  runMockTaskServiceConnectionMisconfigured,
-} from './fixtures'
+import {BASE_INPUTS, CUSTOM_PUBLIC_IDS, CUSTOM_SITE, CUSTOM_SUBDOMAIN, expectSpy, runScenario} from './fixtures'
 
 const BASE_CONFIG = synthetics.DEFAULT_COMMAND_CONFIG
 
 describe('Test suite', () => {
   test('succeeds when app and api keys are given', async () => {
-    const task = await runMockTaskApiKeys()
+    const result = await runScenario('api-keys')
 
-    expectSpy(task, synthetics.executeTests).toHaveBeenCalledWith(expect.anything(), {
+    expectSpy(result, synthetics.executeTests).toHaveBeenCalledWith(expect.anything(), {
       ...BASE_CONFIG,
       ...BASE_INPUTS,
       publicIds: CUSTOM_PUBLIC_IDS,
     })
 
-    expect(task.succeeded).toBe(true)
-    expect(task.warningIssues.length).toEqual(0)
-    expect(task.errorIssues.length).toEqual(0)
+    expect(result.succeeded).toBe(true)
+    expect(result.warningIssues.length).toEqual(0)
+    expect(result.errorIssues.length).toEqual(0)
   })
 
   test('fails when service connection has empty app or api keys', async () => {
-    const task = await runMockTaskServiceConnectionMisconfigured()
+    const result = await runScenario('service-connection-misconfigured')
 
-    expect(task.succeeded).toBe(false)
-    expect(task.warningIssues.length).toEqual(0)
-    expect(task.errorIssues.length).toEqual(2)
-    expect(task.errorIssues[0]).toEqual(
+    expect(result.succeeded).toBe(false)
+    expect(result.warningIssues.length).toEqual(0)
+    expect(result.errorIssues.length).toEqual(2)
+    expect(result.errorIssues[0]).toEqual(
       'Missing API or APP keys to initialize datadog-ci! Check your Datadog service connection.'
     )
 
     // We want this to appear, and the task to fail when an uncaught error is detected.
-    expect(task.errorIssues[1]).toEqual('An uncaught error occurred.')
-    expect(task.stderr).toMatch('[UNCAUGHT_ERROR] Error: Endpoint auth data not present: my service connection')
+    expect(result.errorIssues[1]).toEqual('An uncaught error occurred.')
+    expect(result.stderr).toMatch('[UNCAUGHT_ERROR] Error: Endpoint auth data not present: my service connection')
   })
 
   test('succeeds when service connection has app and api keys', async () => {
-    const task = await runMockTaskServiceConnection()
+    const result = await runScenario('service-connection')
 
-    expectSpy(task, synthetics.executeTests).toHaveBeenCalledWith(expect.anything(), {
+    expectSpy(result, synthetics.executeTests).toHaveBeenCalledWith(expect.anything(), {
       ...BASE_CONFIG,
       ...BASE_INPUTS,
       publicIds: CUSTOM_PUBLIC_IDS,
@@ -65,15 +53,15 @@ describe('Test suite', () => {
       },
     })
 
-    expect(task.succeeded).toBe(true)
-    expect(task.warningIssues.length).toEqual(0)
-    expect(task.errorIssues.length).toEqual(0)
+    expect(result.succeeded).toBe(true)
+    expect(result.warningIssues.length).toEqual(0)
+    expect(result.errorIssues.length).toEqual(0)
   })
 
   test('succeeds with a service connection set up with env vars', async () => {
-    const task = await runMockTaskServiceConnectionEnvVars()
+    const result = await runScenario('service-connection-env-vars')
 
-    expectSpy(task, synthetics.executeTests).toHaveBeenCalledWith(expect.anything(), {
+    expectSpy(result, synthetics.executeTests).toHaveBeenCalledWith(expect.anything(), {
       ...BASE_CONFIG,
       ...BASE_INPUTS,
       datadogSite: CUSTOM_SITE,
@@ -81,23 +69,23 @@ describe('Test suite', () => {
       subdomain: CUSTOM_SUBDOMAIN,
     })
 
-    expect(task.succeeded).toBe(true)
-    expect(task.warningIssues.length).toEqual(0)
-    expect(task.errorIssues.length).toEqual(0)
+    expect(result.succeeded).toBe(true)
+    expect(result.warningIssues.length).toEqual(0)
+    expect(result.errorIssues.length).toEqual(0)
   })
 
   test('succeeds and generates a jUnit report', async () => {
-    const task = await runMockTaskJUnitReport()
+    const result = await runScenario('junit-report')
 
-    expectSpy(task, synthetics.executeTests).toHaveBeenCalledWith(expect.anything(), {
+    expectSpy(result, synthetics.executeTests).toHaveBeenCalledWith(expect.anything(), {
       ...BASE_CONFIG,
       ...BASE_INPUTS,
       publicIds: CUSTOM_PUBLIC_IDS,
     })
 
-    expect(task.succeeded).toBe(true)
-    expect(task.warningIssues.length).toEqual(0)
-    expect(task.errorIssues.length).toEqual(0)
+    expect(result.succeeded).toBe(true)
+    expect(result.warningIssues.length).toEqual(0)
+    expect(result.errorIssues.length).toEqual(0)
 
     expect(fs.existsSync('./reports/TEST-1.xml')).toBe(true)
 
@@ -107,17 +95,17 @@ describe('Test suite', () => {
   })
 
   test('pollingTimeout input overrides the default config', async () => {
-    const task = await runMockTaskPollingTimeout()
+    const result = await runScenario('polling-timeout')
 
-    expectSpy(task, synthetics.executeTests).toHaveBeenCalledWith(expect.anything(), {
+    expectSpy(result, synthetics.executeTests).toHaveBeenCalledWith(expect.anything(), {
       ...BASE_CONFIG,
       ...BASE_INPUTS,
       batchTimeout: 3600000,
       publicIds: CUSTOM_PUBLIC_IDS,
     })
 
-    expect(task.succeeded).toBe(true)
-    expect(task.warningIssues.length).toEqual(0)
-    expect(task.errorIssues.length).toEqual(0)
+    expect(result.succeeded).toBe(true)
+    expect(result.warningIssues.length).toEqual(0)
+    expect(result.errorIssues.length).toEqual(0)
   })
 })
